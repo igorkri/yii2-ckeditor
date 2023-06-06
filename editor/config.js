0a33973b081bef4232134a2e39761b259e2041db
igorkri <igorkri26@gmail.com>
2023-06-06 19:10:15 +0300

    skins plugin

--- editor/config.js
+++ editor/config.js
@@ -1,14 +1,17 @@
 /**
  * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * config.extraPlugins = "youtube,codemirror,video",
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
-	// config.uiColor = '#6e86dc';
-	// config.toolbar = 'Basic';
+    //config.uiColor = '#6e86dc';
+    // config.toolbar = 'Basic';
     config.useComputedState = true,
-    config.extraPlugins = 'video',
-    config.allowedContent = true; /* all tags */
+    config.allowedContent = true, /* all tags */
+    //config.skin = 'n1theme';
+    config.skin = 'moono-lisa',
+   config.extraPlugins = "youtube,codemirror,video"
 };
